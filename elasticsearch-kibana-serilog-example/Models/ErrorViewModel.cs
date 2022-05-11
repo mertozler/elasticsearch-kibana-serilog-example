807e3f2fc52d9d5c045bbed8dbbb937b67961871
mertozler <meozler@gmail.com>
2022-05-11 13:57:54 +0300

    initial commit

---
+++ elasticsearch-kibana-serilog-example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace elasticsearch_kibana_serilog_example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
